c9509e3f7acbcb25b1a9c2565cf9cef5e740250e
Elmasry-631 <irahimelmasry631@gmail.com>
2026-05-20 13:24:37 +0300

    upload sale kitchen reciept module

--- sale_kitchen_receipt/__manifest__.py
+++ sale_kitchen_receipt/__manifest__.py
@@ -11,8 +11,8 @@
         "account",
     ],
     "data": [
-        "reports/kitchen_receipt.xml",  # يجب أن يكون التقرير أولاً ليتم تعريفه في النظام
-        "views/sale_order_views.xml",    # الواجهات ثانياً لتجد الأكستيرنال آي دي جاهزاً
+        "views/sale_order_views.xml",
+        "reports/kitchen_receipt.xml",
     ],
     "installable": True,
     "application": False,
