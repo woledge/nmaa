f0f54ffc1a80448fa535db2d4594d9017462aec1
Elmasry-631 <irahimelmasry631@gmail.com>
2026-07-25 12:24:57 +0300

    uploud modified warehouse_user_access

--- warehouse_user_access/models/stock_models.py
+++ warehouse_user_access/models/stock_models.py
@@ -27,19 +27,6 @@ class StockPicking(models.Model):
         )
 
 
-class StockQuant(models.Model):
-    _inherit = 'stock.quant'
-
-    @api.model
-    def _search(self, domain, offset=0, limit=None, order=None):
-        user = self.env.user
-        if not user.has_group('base.group_system') and user.warehouse_ids:
-            domain.append(('warehouse_id', 'in', user.warehouse_ids.ids))
-        return super(StockQuant, self)._search(
-            domain, offset=offset, limit=limit, order=order
-        )
-
-
 class StockPickingType(models.Model):
     _inherit = 'stock.picking.type'
 
@@ -51,3 +38,10 @@ class StockPickingType(models.Model):
         return super(StockPickingType, self)._search(
             domain, offset=offset, limit=limit, order=order
         )
+
+
+# ملاحظة: تم حذف override الخاص بـ stock.quant لأنه كان يستخدم الحقل
+# 'warehouse_id' الذي غير موجود على stock.quant (يستخدم stock.quant الحقل
+# location_id للربط بالمستودع بشكل غير مباشر). تم استبداله بـ record rule
+# في ملف security/warehouse_user_access_security.xml يستخدم
+# location_id مع child_of view_location_id.
